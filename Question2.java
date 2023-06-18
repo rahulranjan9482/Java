68cc46d4de1bec53ec29ede14f0bd17074724fcf
Rahul Ranjan <116420953+rahulranjan9482@users.noreply.github.com>
2023-06-18 11:59:50 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,12 @@
+// Q:- Write a programming to print triangle using star pattern programming logic .
+public class Question2 {
+    public static void main(String[] args) {
+        int n =7;
+        for(int i=0 ;i<n;i++){
+            for(int j=1; j<i;j++){
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
