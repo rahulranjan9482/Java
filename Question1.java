7bdea6734c02fefe165998428dbc91a871a74a38
Rahul Ranjan <116420953+rahulranjan9482@users.noreply.github.com>
2023-06-18 11:58:49 +0530

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,8 @@
+// Q:- Write a program to print Alphanbets A to H using pattern programming logic.
+public class Question1{
+    public static void main(String[] args) {
+        for( int i= 65 ; i<=72 ; i++){
+            System.out.print((char)i);
+        }
+    }
+}
