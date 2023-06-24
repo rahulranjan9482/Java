687d800d9a665ef9e1c5b0f11f6e40f6bac75ae3
Rahul Ranjan <116420953+rahulranjan9482@users.noreply.github.com>
2023-06-24 07:55:45 +0530

    Add files via upload

---
+++ Question50.java
@@ -0,0 +1,10 @@
+//Q:- write a java program to print to face.
+public class Question50 {
+    public static void main(String[] args) {
+        System.out.println("+|| || || ||+");
+        System.out.println("|  o     o  |");
+        System.out.println("|     ^     |");
+        System.out.println("|    |-|    |");
+        System.out.println("+-----------+");
+    }
+}
